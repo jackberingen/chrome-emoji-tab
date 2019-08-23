b08d8fbdb6d543d102d10bfe89a043b427c16b2f
Jack <jack.ber.private@gmail.com>
2019-08-23 19:39:21 +1000

    fixed stuff

--- canvas.js
+++ canvas.js
@@ -2,6 +2,29 @@ function rnd(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+document.getElementById('moreEmojis').onclick = ()=>{
+	let bodies = [];
+	for(var i = 0; i < 10; i++){
+    var texture = "emoji/" + Math.floor(Math.random() * 100) + ".png";
+		var opts = {
+			angle: rnd(-1.2, 1.2),
+			render: {
+				sprite: {
+					texture: texture,
+					yScale: 0.5,
+					xScale: 0.5
+				}
+			}
+		};
+		var body = Bodies.circle(container.clientWidth / 2 + rnd(-50, 50), -300 + Math.floor(Math.random() * 20), 30.5, opts);
+		Body.setAngularVelocity(body, 0.5);
+		body.restitution = 0.8;
+		bodies.push(body);
+	}
+
+	World.add(engine.world, bodies);
+}
+
 $("#search").keyup(function(event) {
 	if (event.keyCode === 13) {
 		if($("#search").val()){
@@ -33,7 +56,8 @@ var render = Render.create({
     	width: container.clientWidth,
     	wireframes: false,
     	background: 'transparent',
-    	showAngleIndicator: false
+			showAngleIndicator: false,
+			pixelRatio: 'auto'
     }
 });
 
@@ -45,24 +69,26 @@ World.add(engine.world, mouseConstraint);
 
 var bodies = [];
 for(var i = 0; i < 50; i++){
+    var texture = "emoji/" + Math.floor(Math.random() * 100) + ".png";
 	var opts = {
 		angle: rnd(-1.2, 1.2),
 		render: {
-			sprite: { 
-				texture:"emoji/" + Math.floor(Math.random() * 100) + ".png",
+			sprite: {
+				texture: texture,
 				yScale: 0.5,
 				xScale: 0.5
 			}
-		} 
+		}
 	};
 	var body = Bodies.circle(container.clientWidth / 2 + rnd(-50, 50), -300 + Math.floor(Math.random() * 20), 30.5, opts);
 	Body.setAngularVelocity(body, 0.5);
+	body.restitution = 0.8;
 	bodies.push(body);
 }
 
 var wallopts = {
-	isStatic: true, 
-	render: { 
+	isStatic: true,
+	render: {
 		visible: true
 	}
 };
@@ -70,7 +96,7 @@ var wallopts = {
 //Push walls
 bodies.push(Bodies.rectangle(0, container.clientHeight + 10, container.clientWidth * 2, 20, wallopts));
 bodies.push(Bodies.rectangle(-10, -200, 20, container.clientHeight * 4, wallopts));
-bodies.push(Bodies.rectangle(container.clientHeight * 2 + 40, -200, 20, container.clientHeight * 4, wallopts));
+bodies.push(Bodies.rectangle(container.clientWidth + 10, -200, 20, container.clientHeight * 4, wallopts));
 
 // add all of the bodies to the world
 World.add(engine.world, bodies);
